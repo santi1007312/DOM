a1d93e7af05dcbdb5e9037a96829f4dfe4f7932a
Santiago C. <santiagocalvete696@gmail.com>
2026-04-13 09:35:20 -0500

    feat(users): implementar validacion estricta con regex para creacion

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,7 +4,7 @@ import { getTasksByUser } from '../controllers/tasks.controller.js';
 import { verifyToken, isAdmin } from '../middlewares/auth.middleware.js';
 // CORRECCIÓN: Importación del middleware y el esquema
 import { validateSchema } from '../middlewares/validate.middleware.js';
-import { updateUserSchema } from '../schemas/user.schema.js';
+import { updateUserSchema, createUserSchema } from '../schemas/user.schema.js';
 
 const usersRouter = express.Router();
 
@@ -14,7 +14,7 @@ usersRouter.get('/:userId/tasks', verifyToken, getTasksByUser);
 // CRUD DE USUARIOS (Solo Admin)
 usersRouter.get('/', verifyToken, isAdmin, getUsers);
 usersRouter.get('/:id', verifyToken, isAdmin, getUserById);
-usersRouter.post('/', verifyToken, isAdmin, createUser);
+usersRouter.post('/', verifyToken, isAdmin,validateSchema(createUserSchema), createUser);
 // CORRECCIÓN: Se inyecta el middleware en el PUT
 usersRouter.put('/:id', verifyToken, isAdmin, validateSchema(updateUserSchema), updateUser);
 usersRouter.delete('/:id', verifyToken, isAdmin, deleteUser);
--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -11,3 +11,23 @@ export const updateUserSchema = z.object({
     errorMap: () => ({ message: "El estado debe ser 'activo' o 'inactivo'" }) 
   }).optional()
 }).strict("No se permiten campos adicionales en la actualización de usuario");
+
+export const createUserSchema = z.object({
+  name: z.string({ 
+    required_error: "El nombre es obligatorio" 
+  }).min(3, "El nombre debe tener al menos 3 caracteres").max(100),
+  
+  email: z.email("Formato de correo electrónico inválido"),
+
+  document: z.string({ 
+    required_error: "El documento es obligatorio" 
+  }).regex(/^\d+$/, "El documento solo puede contener números, sin espacios ni letras"),
+  
+  role: z.enum(["user", "admin"], { 
+    errorMap: () => ({ message: "El rol debe ser 'user' o 'admin'" }) 
+  }).optional(),
+  
+  status: z.enum(["activo", "inactivo"], { 
+    errorMap: () => ({ message: "El estado debe ser 'activo' o 'inactivo'" }) 
+  }).optional()
+}).strict("No se permiten campos adicionales en la creación de usuario");
